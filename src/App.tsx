74353ed4d1ff1c533cf91e252c9115a03fe53638
Nicholas Charriere <nicholascharriere@gmail.com>
2024-05-13 17:09:33 -0700

    Add format command and run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,12 @@ export default function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }

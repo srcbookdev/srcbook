f4532f75d13a28a864c636ef0efcf88715470245
Ben Reinhart <benjreinhart@gmail.com>
2024-05-18 19:55:45 -0700

    Fix bug with filename validation

--- server/index.mjs
+++ server/index.mjs
@@ -102,13 +102,16 @@ app.post('/sessions/:id/cells', cors(), async (req, res) => {
 
 function validateFilename(session, cellId, filename) {
   const validFormat = /^[a-zA-Z0-9_-]+\.(mjs|json)$/.test(filename);
-  const unique = session.cells.some((cell) => {
-    return cell.id !== cellId && cell.filename === filename;
-  });
 
   if (!validFormat) {
     return 'Invalid filename: filename must consist of letters, numbers, underscores, dashes and must end with mjs or json';
   }
+
+  const unique = session.cells.some((cell) => {
+    // If a different cell with the same filename exists,
+    // this is not a unique filename within the session.
+    return cell.id === cellId || cell.filename !== filename;
+  });
 
   if (!unique) {
     return 'Invalid filename: filename is not unique';

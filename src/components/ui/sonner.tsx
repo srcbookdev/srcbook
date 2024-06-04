cebc18ca8ef7047ca042a85e392e3fcfcad06c91
Nicholas Charriere <nicholascharriere@gmail.com>
2024-06-03 21:11:09 -0700

    Remove next-themes, no next over here

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,14 +1,10 @@
-import { useTheme } from 'next-themes';
 import { Toaster as Sonner } from 'sonner';
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = 'system' } = useTheme();
-
   return (
     <Sonner
-      theme={theme as ToasterProps['theme']}
       className="toaster group"
       toastOptions={{
         classNames: {

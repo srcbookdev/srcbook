9f945b4fb926b277d0b0fdc09c5b7526719789ec
Ben Reinhart <benjreinhart@gmail.com>
2024-06-19 12:49:07 -0700

    Light/dark mode setting (#78)

--- packages/web/src/routes/settings.tsx
+++ packages/web/src/routes/settings.tsx
@@ -10,6 +10,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+import useTheme from '@/components/use-theme';
+import { Switch } from '@/components/ui/switch';
 
 async function loader() {
   const [{ result: config }, { result: diskResult }] = await Promise.all([getConfig(), disk({})]);
@@ -32,31 +34,43 @@ function Settings() {
     await updateConfig({ defaultLanguage: value });
   };
 
-  return (
-    <div className="space-y-10">
-      <div>
-        <h1 className="text-2xl pb-4">Settings</h1>
-        <h2 className="text-xl pb-2">Base Directory</h2>
-        <label className="opacity-70">
-          The default directory to look for Srcbooks when importing.
-        </label>
-        <DirPicker dirname={baseDir} entries={entries} cta="Change" />
-      </div>
+  const { theme, toggleTheme } = useTheme();
 
-      <div>
-        <h2 className="text-xl pb-2">Default Language</h2>
-        <label className="opacity-70 block pb-2">
-          The default language to use when creating new Srcbooks.
-        </label>
-        <Select onValueChange={updateDefaultLanguage}>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder={defaultLanguage} />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="typescript">typescript</SelectItem>
-            <SelectItem value="javascript">javascript</SelectItem>
-          </SelectContent>
-        </Select>
+  return (
+    <div>
+      <h1 className="text-4xl pb-4">Settings</h1>
+      <div className="space-y-10">
+        <div>
+          <h2 className="text-xl pb-2">Base Directory</h2>
+          <label className="opacity-70">
+            The default directory to look for Srcbooks when importing.
+          </label>
+          <DirPicker dirname={baseDir} entries={entries} cta="Change" />
+        </div>
+
+        <div>
+          <h2 className="text-xl pb-2">Theme</h2>
+          <div className="flex items-center gap-2">
+            <Switch checked={theme === 'dark'} onCheckedChange={toggleTheme} />
+            <label>Dark mode</label>
+          </div>
+        </div>
+
+        <div>
+          <h2 className="text-xl pb-2">Default Language</h2>
+          <label className="opacity-70 block pb-2">
+            The default language to use when creating new Srcbooks.
+          </label>
+          <Select onValueChange={updateDefaultLanguage}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder={defaultLanguage} />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="typescript">typescript</SelectItem>
+              <SelectItem value="javascript">javascript</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
       </div>
     </div>
   );

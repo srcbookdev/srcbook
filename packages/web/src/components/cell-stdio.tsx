a3e7f2499e3a2e21dbd04169208ccc5ebecc299c
Nicholas Charriere <nicholascharriere@gmail.com>
2024-06-24 13:45:30 -0700

    Permanent footer on code cells and package.json cell when visible

--- packages/web/src/components/cell-stdio.tsx
+++ packages/web/src/components/cell-stdio.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Ban, X } from 'lucide-react';
+import { Ban, PanelBottomClose, PanelBottomOpen } from 'lucide-react';
 import { CodeCellType, PackageJsonCellType } from '@srcbook/shared';
 import { cn } from '@/lib/utils';
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/underline-flat-tabs';
@@ -21,22 +21,34 @@ export function CellStdio({ cell, show, setShow }: PropsType) {
   const stderr = getOutput(cell.id, 'stderr') as StderrOutputType[];
   const tscOutput = getOutput(cell.id, 'tsc') as TscOutputType[];
 
-  if (!show) {
-    return null;
-  }
-
-  function dismiss() {
-    clearOutput(cell.id);
-    setShow(false);
-  }
-
   return (
     <div className="border-t font-mono text-sm">
       <Tabs value={activeTab} onValueChange={setActiveTab}>
-        <div className="px-3 flex items-center justify-between bg-muted text-tertiary-foreground border-b">
-          <TabsList className="h-full">
-            <TabsTrigger value="stdout">stdout</TabsTrigger>
-            <TabsTrigger value="stderr">
+        <div
+          className={cn(
+            'px-3 flex items-center justify-between bg-muted text-tertiary-foreground rounded-md',
+            show && 'border-b rounded-none',
+          )}
+        >
+          <TabsList className={cn('h-full', !show && '')}>
+            <TabsTrigger
+              onClick={() => setShow(true)}
+              value="stdout"
+              className={cn(
+                !show &&
+                'border-transparent data-[state=active]:border-transparent data-[state=active]:text-tertiary-foreground mb-0',
+              )}
+            >
+              stdout
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => setShow(true)}
+              value="stderr"
+              className={cn(
+                !show &&
+                'border-transparent data-[state=active]:border-transparent data-[state=active]:text-tertiary-foreground mb-0',
+              )}
+            >
               {stderr.length > 0 ? (
                 <>
                   stderr <span className="text-sb-red-30">({stderr.length})</span>
@@ -46,7 +58,14 @@ export function CellStdio({ cell, show, setShow }: PropsType) {
               )}
             </TabsTrigger>
             {cell.type === 'code' && cell.language === 'typescript' && (
-              <TabsTrigger value="problems">
+              <TabsTrigger
+                onClick={() => setShow(true)}
+                value="problems"
+                className={cn(
+                  !show &&
+                  'border-transparent data-[state=active]:border-transparent data-[state=active]:text-tertiary-foreground mb-0',
+                )}
+              >
                 {tscOutput.length > 0 ? (
                   <>
                     problems <span className="text-sb-red-30">({tscOutput.length})</span>
@@ -61,21 +80,25 @@ export function CellStdio({ cell, show, setShow }: PropsType) {
             <button className=" hover:text-secondary-hover" onClick={() => clearOutput(cell.id)}>
               <Ban size={16} />
             </button>
-            <button className=" hover:text-secondary-hover" onClick={dismiss}>
-              <X size={20} />
+            <button className=" hover:text-secondary-hover" onClick={() => setShow(!show)}>
+              {show ? <PanelBottomOpen size={20} /> : <PanelBottomClose size={20} />}
             </button>
           </div>
         </div>
-        <TabsContent value="stdout" className="mt-0">
-          <Stdout stdout={stdout} />
-        </TabsContent>
-        <TabsContent value="stderr" className="mt-0">
-          <Stderr stderr={stderr} />
-        </TabsContent>
-        {cell.type === 'code' && cell.language === 'typescript' && (
-          <TabsContent value="problems" className="mt-0">
-            <TscOutput tsc={tscOutput} />
-          </TabsContent>
+        {show && (
+          <>
+            <TabsContent value="stdout" className="mt-0">
+              <Stdout stdout={stdout} />
+            </TabsContent>
+            <TabsContent value="stderr" className="mt-0">
+              <Stderr stderr={stderr} />
+            </TabsContent>
+            {cell.type === 'code' && cell.language === 'typescript' && (
+              <TabsContent value="problems" className="mt-0">
+                <TscOutput tsc={tscOutput} />
+              </TabsContent>
+            )}
+          </>
         )}
       </Tabs>
     </div>
@@ -121,7 +144,7 @@ function TscOutput({ tsc }: { tsc: TscOutputType[] }) {
   return (
     <div className="p-2 flex flex-col-reverse max-h-96 overflow-scroll whitespace-pre-wrap">
       {tsc.length === 0 ? (
-        <div className="italic text-center text-muted-foreground">No output</div>
+        <div className="italic text-center text-muted-foreground">No problems</div>
       ) : (
         formatOutput(tsc)
       )}

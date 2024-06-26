b5bf6fddcf569539ca03c9b4f028d938ee684053
Ben Reinhart <benjreinhart@gmail.com>
2024-06-26 13:01:00 -0700

    Rework build and deploy flow (#114)
    * Remove vendor and rename directories
    
    * Include examples in dist output
    
    * Add clear back in
    
    * Put header back in place

--- srcbook/index.mjs
+++ srcbook/index.mjs
@@ -1,10 +1,16 @@
+// This is important. It will create necessary directories on the file system. Import this first.
+import './lib/initialization.mjs';
+
+// This is important. It will create and setup the database. Import this second.
+import './lib/db/index.mjs';
+
 /**
  * Run the Srcbook application.
+ *
  * For this, we need to:
- *  - Ensure the ~/.srcbook directory and its /srcbooks/ child directory exist
- *  - Serve the react web app frontend
- *  - Start the websocket server
  *  - Serve the API
+ *  - Serve the WebSocket server
+ *  - Serve the React frontend
  *
  */
 import readline from 'node:readline';
@@ -13,12 +19,8 @@ import http from 'node:http';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { WebSocketServer as WsWebSocketServer } from 'ws';
-import { wss, app } from './vendor/dist/index.mjs';
+import { wss, app } from './lib/index.mjs';
 import chalk from 'chalk';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const __client_dist = path.join(__dirname, '/web_dist/');
 
 function clearScreen() {
   const repeatCount = process.stdout.rows - 2;
@@ -30,9 +32,17 @@ function clearScreen() {
 
 clearScreen();
 
+console.log(chalk.bgGreen.black('  Srcbook  '));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PUBLIC_DIR = path.join(__dirname, 'public');
+const INDEX_HTML = path.join(PUBLIC_DIR, 'index.html');
+
 // Serve the static files, compiled from the packages/web/ React app
 console.log(chalk.dim('Serving static files (React app)...'));
-app.use(express.static(__client_dist));
+app.use(express.static(PUBLIC_DIR));
 const server = http.createServer(app);
 
 // Create the WebSocket server
@@ -41,9 +51,7 @@ const webSocketServer = new WsWebSocketServer({ server });
 webSocketServer.on('connection', wss.onConnection);
 
 // Serve the react-app for all other routes, handled by client-side routing
-app.get('*', (_req, res) => {
-  res.sendFile(path.join(__client_dist, 'index.html'));
-});
+app.get('*', (_req, res) => res.sendFile(INDEX_HTML));
 
 console.log(chalk.green('Initialization complete.'));
 
--- srcbook/vendor/drizzle/0001_favorite_language.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `config` RENAME COLUMN `baseDir` TO `base_dir`;--> statement-breakpoint
-ALTER TABLE `config` ADD `default_language` text DEFAULT 'typescript' NOT NULL;

28376fbc3f9a45352f5c71a4d9796a1e0880e223
Ben Reinhart <benjreinhart@gmail.com>
2024-07-08 20:45:15 -0700

    Remove lint warning

--- packages/web/src/components/use-effect-once.tsx
+++ packages/web/src/components/use-effect-once.tsx
@@ -16,6 +16,7 @@ function useEffectDev(effect: EffectCallback) {
 }
 
 function useEffectProd(effect: EffectCallback) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(effect, []);
 }
 

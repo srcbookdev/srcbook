b8e6ac24d7dda3660d4de8a863a68cee7141fce6
Nicholas Charriere <nicholascharriere@gmail.com>
2024-07-11 13:42:38 -0700

    Design tweaks (#126)

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -135,6 +135,7 @@ module.exports = {
       },
       borderRadius: {
         sm: '0.1875rem', // 3px
+        mid: '0.375rem', // 6px
         md: '0.4375rem', // 7px
       },
       transitionProperty: {

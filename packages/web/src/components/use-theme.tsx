a0dec9191158571cb296e7f4f4532d52b8dacade
Ben Reinhart <benjreinhart@gmail.com>
2024-07-22 12:47:14 -0700

    Simplify useTheme imlpementation

--- packages/web/src/components/use-theme.tsx
+++ packages/web/src/components/use-theme.tsx
@@ -31,14 +31,9 @@ function updateClass(theme: ThemeType) {
 export default function useTheme() {
   const [theme, _setTheme] = useState<ThemeType>(getTheme());
 
-  const [codeTheme, setCodeTheme] = useState<CodeThemeType>(
-    getTheme() === 'dark' ? srcbookDark : srcbookLight,
-  );
-
   function setTheme(theme: ThemeType) {
     updateClass(theme);
     persistTheme(theme);
-    setCodeTheme(theme === 'dark' ? srcbookDark : srcbookLight);
     _setTheme(theme);
   }
 
@@ -46,5 +41,10 @@ export default function useTheme() {
     setTheme(theme === 'dark' ? 'light' : 'dark');
   }
 
-  return { theme, codeTheme, toggleTheme, setTheme };
+  return {
+    theme,
+    codeTheme: getTheme() === 'dark' ? srcbookDark : srcbookLight,
+    toggleTheme,
+    setTheme,
+  };
 }

42f66f5a0c7a16673a8cdfc07949a7d392e16b73
Aswanth <57211637+Aswanth-c@users.noreply.github.com>
2024-08-27 13:35:47 -0700

    fix: Limit title length to 50 characters in cellUpdate function (#212)
    * fix: Limit title length to 50 characters in cellUpdate function
    
    * added type validation on useState
    
    * added necessary pr changes
    
    * added the extra line that was removed
    
    * changed heading length to 44
    
    * Moved back to h1 instead of text area
    
    handled edge cases
    
    * added pr changes
    including import existing zod function
    
    * fixed a typo

--- packages/web/src/components/ui/heading.tsx
+++ packages/web/src/components/ui/heading.tsx
@@ -1,36 +1,69 @@
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
+import { Info } from 'lucide-react';
+import { TitleCellUpdateAttrsSchema } from '@srcbook/shared';
 
 const className =
-  'flex w-full rounded-md border border-transparent bg-transparent px-1 py-1 transition-colors hover:border-input hover:shadow-sm focus-visible:shadow-md focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50';
+  'flex w-full break-all whitespace-normal rounded-md border border-transparent bg-transparent px-1 py-1 transition-colors hover:border-input hover:shadow-sm focus-visible:shadow-md focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50';
 
 export function EditableH1(props: {
   text: string;
   className?: string;
   onUpdated: (text: string) => void;
 }) {
-  const ref = React.useRef<HTMLHeadingElement | null>(null);
+  const ref = React.useRef<HTMLHeadingElement>(null);
+  const [isMaxHeadingLengthExceeded, setIsMaxHeadingLengthExceeded] =
+    React.useState<boolean>(false);
+
+  const maxHeadingLength = 44;
+
+  const handleChange = (newValue: string) => {
+    const result = TitleCellUpdateAttrsSchema.safeParse({ text: newValue });
+    if (!result.success) {
+      setIsMaxHeadingLengthExceeded(true);
+      if (ref.current) {
+        // Ensures text is saved when contenteditable becomes false  by triggering blur event
+        ref.current.blur();
+      }
+      setTimeout(() => {
+        setIsMaxHeadingLengthExceeded(false);
+      }, 2000);
+      return;
+    }
+  };
 
   return (
-    <h1
-      className={cn(className, props.className)}
-      ref={ref}
-      contentEditable
-      suppressContentEditableWarning={true}
-      onBlur={(e) => {
-        const text = e.currentTarget.innerHTML;
-        if (text !== props.text) {
-          props.onUpdated(text);
-        }
-      }}
-      onKeyDown={(e) => {
-        if (e.key === 'Enter' && ref.current) {
-          ref.current.blur();
-        }
-      }}
-    >
-      {props.text}
-    </h1>
+    <div>
+      <h1
+        className={cn(className, props.className)}
+        ref={ref}
+        contentEditable={!isMaxHeadingLengthExceeded}
+        suppressContentEditableWarning={true}
+        onBlur={(e) => {
+          e.currentTarget.innerHTML = e.currentTarget.innerHTML.slice(0, maxHeadingLength);
+          const text = e.currentTarget.innerHTML;
+          if (text !== props.text) {
+            props.onUpdated(text);
+          }
+        }}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' && ref.current) {
+            ref.current.blur();
+          }
+        }}
+        onInput={(e) => {
+          handleChange(e.currentTarget.innerText);
+        }}
+      >
+        {props.text}
+      </h1>
+      {isMaxHeadingLengthExceeded && (
+        <div className="bg-error text-error-foreground flex items-center rounded-sm border border-transparent px-[10px] py-2 text-sm leading-none font-medium">
+          <Info size={14} className="mr-1.5" />
+          Max heading length exceeded
+        </div>
+      )}
+    </div>
   );
 }

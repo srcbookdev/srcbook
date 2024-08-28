e378d6e69b878cf985e0374c809b858e5d6189ca
Nicholas Charriere <nicholascharriere@gmail.com>
2024-08-28 14:44:30 -0700

    Fix build in ui-sdk package

---
+++ packages/ui-sdk/index.ts
@@ -0,0 +1,5 @@
+export { io } from './src/io.js';
+export type { RenderUpdate, Renderer, RenderComponent } from './src/renderer.js';
+export { IOComponent } from './src/IOComponent.js';
+export { Action } from './src/Action.js';
+export { UIApp } from './src/UIApp.js';
--- packages/ui-sdk/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { io } from './io.js';
-export type { RenderUpdate, Renderer, RenderComponent } from './renderer.js';
-export { IOComponent } from './IOComponent.js';
-export { Action } from './Action.js';
-export { UIApp } from './UIApp.js';

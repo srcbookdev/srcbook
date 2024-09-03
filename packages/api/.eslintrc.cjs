35a5e2533e49aa4a88840ae87a3ee3c9d9fe731b
versecafe <147033096+versecafe@users.noreply.github.com>
2024-09-03 17:53:48 -0500

    turbo eslint for packages (#248)

---
+++ packages/api/.eslintrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: [require.resolve('@srcbook/configs/eslint/library.js')],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.lint.json',
+    tsconfigRootDir: __dirname,
+  },
+  globals: {
+    Bun: false,
+  },
+};

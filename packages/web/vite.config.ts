f797577869955235e78646e0ebe0230f2e1e1247
swk777 <swk777@gmail.com>
2024-09-12 19:35:40 +0800

    support server hmr

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,10 +1,29 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import chokidar from 'chokidar';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'backend-watch-reload',
+      configureServer(server) {
+        const watcher = chokidar.watch('../api/**/*.mts', {
+          ignored: /node_modules/,
+          persistent: true,
+        });
+        watcher.on('change', (path) => {
+          console.log(`Backend file ${path} changed. Reloading frontend...`);
+          server.ws.send({
+            type: 'full-reload',
+          });
+        });
+      },
+    },
+  ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),

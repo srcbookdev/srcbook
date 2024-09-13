f4264004bab579288b4ebb76ae0571de58024e59
Ryan Gaus <rsg1egoman@gmail.com>
2024-09-13 15:46:50 -0400

    fix: fix secrets_to_sessions not being plural

--- packages/api/drizzle/0009_secret_session_unique.sql
+++ packages/api/drizzle/0009_secret_session_unique.sql
@@ -1,1 +1,1 @@
-CREATE UNIQUE INDEX `secrets_to_sessions_session_id_secret_id_unique` ON `secrets_to_session` (`session_id`,`secret_id`);
+CREATE UNIQUE INDEX `secrets_to_sessions_session_id_secret_id_unique` ON `secrets_to_sessions` (`session_id`,`secret_id`);

9f4357d238f6b35e8dcf1b309c060015644daa49
Ryan Gaus <rsg1egoman@gmail.com>
2024-09-13 16:35:26 -0400

    fix: add on delete cascade to secrets_to_sessions -> secret relation

--- packages/api/drizzle/0008_add_secrets.sql
+++ packages/api/drizzle/0008_add_secrets.sql
@@ -2,5 +2,5 @@ CREATE TABLE `secrets_to_sessions` (
 	`id` integer PRIMARY KEY NOT NULL,
 	`session_id` text NOT NULL,
 	`secret_id` integer NOT NULL,
-	FOREIGN KEY (`secret_id`) REFERENCES `secrets`(`id`) ON UPDATE no action ON DELETE no action
+	FOREIGN KEY (`secret_id`) REFERENCES `secrets`(`id`) ON UPDATE no action ON DELETE CASCADE
 );

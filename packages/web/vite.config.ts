5c3b74da02456fb465883677790aa13431695d0a
Ben Reinhart <benjreinhart@gmail.com>
2024-09-16 10:43:08 -0700

    Revert "support backend hmr (#277)" (#302)
    This reverts commit c14ceab503d629f6072e1621e5aa8f32bf20b810.

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,29 +1,10 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import chokidar from 'chokidar';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    // reload on backend change
-    {
-      name: 'backend-watch-reload',
-      configureServer(server) {
-        const watcher = chokidar.watch('../api/**/*.mts', {
-          ignored: /node_modules/,
-          persistent: true,
-        });
-        watcher.on('change', () => {
-          server.ws.send({
-            type: 'full-reload',
-          });
-        });
-      },
-    },
-  ],
-
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),

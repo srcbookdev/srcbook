5cad1b0589e21808b341f76ed2ffd6b43e7fa360
Ben Reinhart <benjreinhart@gmail.com>
2024-09-18 23:00:11 -0700

    Simplify secrets panel

--- packages/web/src/components/session-menu/secrets-panel.tsx
+++ packages/web/src/components/session-menu/secrets-panel.tsx
@@ -1,8 +1,5 @@
-// import { SessionMenuPanelContentsProps } from '.';
-
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
-import { type SecretWithAssociatedSessions } from '@srcbook/shared';
 
 import {
   getSecrets,
@@ -14,138 +11,73 @@ import { SessionMenuPanelContentsProps } from '.';
 
 type PropsType = Pick<SessionMenuPanelContentsProps, 'session'>;
 
-export default function SessionMenuPanelSecrets(props: PropsType) {
-  const [secretsList, setSecretsList] = useState<
-    | { status: 'idle' }
-    | { status: 'loading' }
-    | { status: 'complete'; data: Array<SecretWithAssociatedSessions> }
-    | { status: 'error' }
-  >({ status: 'idle' });
+export default function SessionMenuPanelSecrets({ session }: PropsType) {
+  const [secrets, setSecrets] = useState<{ name: string; checked: boolean }[]>([]);
+
   useEffect(() => {
     const run = async () => {
-      setSecretsList({ status: 'loading' });
-      let result: Array<SecretWithAssociatedSessions>;
       try {
-        result = (await getSecrets()).result;
+        const { result: secretsWithAssociations } = await getSecrets();
+
+        const secrets = secretsWithAssociations.map((s) => ({
+          name: s.name,
+          checked: s.associatedWithSessionIds.includes(session.id),
+        }));
+
+        setSecrets(secrets);
       } catch (err) {
-        console.error('Error loading secrets!', err);
-        setSecretsList({ status: 'error' });
+        console.error('Error loading secrets', err);
         return;
       }
-
-      setSecretsList({ status: 'complete', data: result });
     };
 
-    // FIXME: it is possible for this to run twice in parallel?
     run();
   }, []);
 
-  // Store a list of all secret name association changes that are in progress so that a user cannot
-  // change an already in flight association
-  const [loadingSecretNames, setLoadingSecretName] = useState<Set<string>>(new Set());
-  const onRegisterLoadingSecretName = useCallback(
-    (secretName: string) => {
-      setLoadingSecretName((old) => {
-        const newSet = new Set(old);
-        newSet.add(secretName);
-        return newSet;
-      });
-    },
-    [setLoadingSecretName],
-  );
-  const onDeregisterLoadingSecretName = useCallback(
-    (secretName: string) => {
-      setLoadingSecretName((old) => {
-        const newSet = new Set(old);
-        newSet.delete(secretName);
-        return newSet;
-      });
-    },
-    [setLoadingSecretName],
-  );
+  function toggleSecret(secretName: string) {
+    setSecrets((secrets) =>
+      secrets.map((s) => {
+        return s.name === secretName ? { ...s, checked: !s.checked } : s;
+      }),
+    );
+  }
 
-  const onChangeSecretEnabled = useCallback(
-    async (secretName: string, enabled: boolean) => {
-      onRegisterLoadingSecretName(secretName);
+  async function onChangeSecretEnabled(secretName: string, enabled: boolean) {
+    toggleSecret(secretName);
 
-      try {
-        if (enabled) {
-          await associateSecretWithSession(props.session.id, secretName);
-        } else {
-          await disassociateSecretWithSession(props.session.id, secretName);
-        }
-      } catch (err) {
-        onDeregisterLoadingSecretName(secretName);
-        console.error(`Error changing secret ${secretName} enabled:`, err);
-        toast.error('Error enabling/disabling secret!');
-        return;
-      }
-
-      // After changing the secret value, optimisitcally update the secrets list
-      // FIXME: maybe it would be better to just refetch?
-      setSecretsList((old) => {
-        if (old.status !== 'complete') {
-          return old;
-        }
-
-        return {
-          ...old,
-          data: old.data.map((item) => {
-            if (item.name === secretName) {
-              return {
-                ...item,
-                associatedWithSessionIds: enabled
-                  ? [...item.associatedWithSessionIds, props.session.id]
-                  : item.associatedWithSessionIds.filter((id) => id !== props.session.id),
-              };
-            } else {
-              return item;
-            }
-          }),
-        };
-      });
-
-      // NOTE: add a slight delay before removing the loading state to minimize ui flicker
-      setTimeout(() => {
-        onDeregisterLoadingSecretName(secretName);
-      }, 50);
-    },
-    [props.session.id, onRegisterLoadingSecretName, onDeregisterLoadingSecretName],
-  );
+    try {
+      const operation = enabled ? associateSecretWithSession : disassociateSecretWithSession;
+      await operation(session.id, secretName);
+    } catch (err) {
+      toggleSecret(secretName);
+      console.error(`Error toggling secret ${secretName}`, err);
+      toast.error('Error enabling/disabling secret!');
+      return;
+    }
+  }
 
   return (
     <>
       <h4 className="text-lg font-semibold leading-tight mb-2">Secrets</h4>
       <h6 className="mb-4 text-tertiary-foreground">Available in this srcbook</h6>
 
-      {secretsList.status === 'idle' || secretsList.status === 'loading' ? (
-        <span>Loading</span>
-      ) : null}
-      {secretsList.status === 'error' ? <span>Error loading secrets!</span> : null}
-      {secretsList.status === 'complete' ? (
-        <div>
-          {secretsList.data.map((secret) => {
-            const checked = secret.associatedWithSessionIds.includes(props.session.id);
-            return (
-              <div
-                className="flex items-center justify-between h-8 cursor-pointer"
-                key={secret.name}
-                onClick={() => onChangeSecretEnabled(secret.name, !checked)}
-                aria-hidden={true}
-              >
-                <span className="font-mono">{secret.name}</span>
-                <div onClick={(e) => e.stopPropagation()} aria-hidden={true}>
-                  <Switch
-                    disabled={loadingSecretNames.has(secret.name)}
-                    checked={checked}
-                    onCheckedChange={(checked) => onChangeSecretEnabled(secret.name, checked)}
-                  />
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      ) : null}
+      <div>
+        {secrets.map((secret) => (
+          <label
+            key={secret.name}
+            htmlFor={`secret-${secret.name}`}
+            className="flex items-center justify-between h-8 font-mono cursor-pointer"
+          >
+            {secret.name}
+
+            <Switch
+              id={`secret-${secret.name}`}
+              checked={secret.checked}
+              onCheckedChange={(checked) => onChangeSecretEnabled(secret.name, checked)}
+            />
+          </label>
+        ))}
+      </div>
     </>
   );
 }

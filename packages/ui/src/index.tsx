23704211293350639f04b6d4244514946adb87ff
Ryan Gaus <rsg1egoman@gmail.com>
2024-09-25 15:42:17 -0400

    feat: add initial @srcbook/ui package

---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,26 @@
+export * from '@/components/ui/button';
+export * from '@/components/ui/card';
+export * from '@/components/ui/collapsible';
+export * from '@/components/ui/command';
+export * from '@/components/ui/dialog';
+export * from '@/components/ui/dropdown-menu';
+export * from '@/components/ui/heading';
+export * from '@/components/ui/input';
+export * from '@/components/ui/navigation-menu';
+export * from '@/components/ui/popover';
+export * from '@/components/ui/resizable';
+export * from '@/components/ui/select';
+export * from '@/components/ui/sheet';
+export * from '@/components/ui/sonner';
+export * from '@/components/ui/switch';
+export * from '@/components/ui/tabs';
+export * from '@/components/ui/textarea';
+export * from '@/components/ui/tooltip';
+export {
+  Tabs as UnderlineTabs,
+  TabsList as UnderlingTabsList,
+  TabsTrigger as UnderlineTabsTrigger,
+  TabsContent as UnderlineTabsContent,
+} from '@/components/ui/underline-flat-tabs';
+
+export * from '@/lib/utils';

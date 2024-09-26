c128c4ffd4b5b807f6c1fb718fb1910d31bd9ff5
Ryan Gaus <rsg1egoman@gmail.com>
2024-09-26 15:15:18 -0400

    feat: move markdown cell into @srcbook/components

---
+++ packages/components/src/components/delete-cell-dialog.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from '@/components/ui/dialog';
+
+export default function DeleteCellWithConfirmationModal({
+  onDeleteCell,
+  children,
+}: {
+  onDeleteCell: () => void;
+  children: React.ReactNode;
+}) {
+  const [open, setOpen] = useState(false);
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Delete this cell</DialogTitle>
+          <DialogDescription>
+            We currently don't support history, are you sure you want to delete it?
+          </DialogDescription>
+          <div className="flex w-full justify-end items-center gap-2 pt-4 bg-background">
+            <Button
+              variant="secondary"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              Cancel
+            </Button>
+            <Button variant="destructive" onClick={() => onDeleteCell()}>
+              Delete
+            </Button>
+          </div>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}

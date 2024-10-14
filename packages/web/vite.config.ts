73cd6e8aa4f7a3a9186a7214fbab2824f047a8e5
swk777 <swk777@gmail.com>
2024-10-15 04:46:38 +0800

    support backend hmr (#326)
    * support server hmr
    
    * add lock file
    
    * remove console
    
    * add comment
    
    * fix  port in use
    
    * add changeset
    
    * dev incremental
    
    * upgrade chokidar to v4 and adapt codebase
    
    * fix format
    
    ---------
    
    Co-authored-by: versecafe <147033096+versecafe@users.noreply.github.com>

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,10 +1,29 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import chokidar from 'chokidar';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // reload on backend change
+    {
+      name: 'backend-watch-reload',
+      configureServer(server) {
+        const watcher = chokidar.watch('../api', {
+          persistent: true,
+          ignored: (file, stats) => !!stats?.isFile() && !file.endsWith('.mts'),
+        });
+        watcher.on('change', () => {
+          server.ws.send({
+            type: 'full-reload',
+          });
+        });
+      },
+    },
+  ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),

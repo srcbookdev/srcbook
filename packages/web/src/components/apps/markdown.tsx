b87e8924b74d4e1e382285981fb8b38a80f74903
Effy <effyyzhang@gmail.com>
2024-10-19 20:16:55 -0600

    update a few spacing (#391)

--- packages/web/src/components/apps/markdown.tsx
+++ packages/web/src/components/apps/markdown.tsx
@@ -3,7 +3,7 @@ import { cn } from '@srcbook/components';
 
 export default function Markdown(props: { source: string; className?: string }) {
   return (
-    <div className={cn('sb-prose-chat', props.className)}>
+    <div className={cn('sb-prose-chat px-2', props.className)}>
       <MarkdownReact gfm>{props.source}</MarkdownReact>
     </div>
   );

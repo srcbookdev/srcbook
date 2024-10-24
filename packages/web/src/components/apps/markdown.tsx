3cb78e2c9b493144e661aff0968a044798d2cccc
Ryan Gaus <rsg1egoman@gmail.com>
2024-10-24 15:42:22 -0400

    fix: revert markdown file changes

--- packages/web/src/components/apps/markdown.tsx
+++ packages/web/src/components/apps/markdown.tsx
@@ -1,11 +1,10 @@
-import { marked } from 'marked';
-import { cn } from '@/lib/utils';
+import MarkdownReact from 'marked-react';
+import { cn } from '@srcbook/components';
 
 export default function Markdown(props: { source: string; className?: string }) {
   return (
-    <div
-      className={cn('sb-prose-chat px-2', props.className)}
-      dangerouslySetInnerHTML={{ __html: marked(props.source) }}
-    />
+    <div className={cn('sb-prose-chat px-2', props.className)}>
+      <MarkdownReact gfm>{props.source}</MarkdownReact>
+    </div>
   );
 }
